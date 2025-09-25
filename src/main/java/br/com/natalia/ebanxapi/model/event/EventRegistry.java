e05f42c86b1efa8700e73711727d91316d55b917
nataliafrancisca <nathifrancisca@gmail.com>
2025-09-24 22:40:42 -0300

    feat: change validation from @Positive for @PositiveOrZero

--- src/main/java/br/com/natalia/ebanxapi/model/event/EventRegistry.java
+++ src/main/java/br/com/natalia/ebanxapi/model/event/EventRegistry.java
@@ -1,13 +1,13 @@
 package br.com.natalia.ebanxapi.model.event;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record EventRegistry(
         @NotNull(message = "must be filled.")
         EventType type,
         @NotNull(message = "must be filled.")
-        @Positive(message = "must be a positive number.")
+        @PositiveOrZero(message = "must be a positive number.")
         Double amount,
         String origin,
         String destination
